34a2f8701cb15f8198679cdbeee09e063af78ba0
wuqi <1303982968@qq.com>
2018-07-18 18:41:03 +0800

    demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,84 @@
+<template>
+<el-menu
+  :default-active="activeIndex2"
+  class="el-menu-demo"
+  mode="horizontal"
+  @select="handleSelect"
+  background-color="#545c64"
+  text-color="#fff"
+  active-text-color="#ffd04b">
+  <el-menu-item index="1">处理中心</el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">我的工作台</template>
+    <el-menu-item index="2-1">选项1</el-menu-item>
+    <el-menu-item index="2-2">选项2</el-menu-item>
+    <el-menu-item index="2-3">选项3</el-menu-item>
+    <el-submenu index="2-4">
+      <template slot="title">选项4</template>
+      <el-menu-item index="2-4-1">选项1</el-menu-item>
+      <el-menu-item index="2-4-2">选项2</el-menu-item>
+      <el-menu-item index="2-4-3">选项3</el-menu-item>
+    </el-submenu>
+  </el-submenu>
+  <el-menu-item index="3" disabled>消息中心</el-menu-item>
+  <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+</el-menu>
+
+</template>
+<script>
+export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+        methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    },
+  components: {},
+
+  computed: {},
+
+  mounted() {
+    
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    color: #333;
+  }
+   .el-select .el-input {
+    width: 130px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
+</style>
+
---
+++ src/components/testmsg.vue
@@ -0,0 +1,70 @@
+<template>
+<div id="test">
+<ul><li><a @click="showdetail('球类')">球类</li>
+    <li><a @click="showdetail('粮食')">粮食</li>
+    <li><a @click="showdetail('衣服')">衣服</li>
+</ul>
+<ul v-for="item in arr" :key="item.id">
+         <li>item.src</li>
+         <li>item.price</li>
+         <li>item.msg</li>
+</ul>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        arr=[],
+      obj: {
+        球类: [
+          {id:1, src: "../image/football.jpg", price: 10, msg: "足球" },
+          { id:2,src: "../image/basketball.jpg", price: 20, msg: "篮球" },
+          { id:3,src: "../image/tennis.jpg", price: 30, msg: "乒乓球" }
+        ],
+        粮食: [
+          {id:1, src: "../image/husked.jpg", price: 20, msg: "大米" },
+          { id:2,src: "../image/corn.jpg", price: 5, msg: "玉米" },
+          { id:3,src: "../image/mung.jpg", price: 10, msg: "绿豆" }
+        ],
+        衣服: [
+          { id:1,src: "../image/T.jpg", price: 40, msg: "T恤" },
+          { id:2,src: "../image/sports.jpg", price: 60, msg: "运动服" },
+          { id:3,src: "../image/coat.jpg", price: 50, msg: "外套" }
+        ]
+      }
+    };
+  },
+  methods: {
+    showdetail(cmsg){
+   this.arr=obj[cmsg]
+    }
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
