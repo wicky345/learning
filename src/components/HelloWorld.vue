3b5711648c88f4a938851d20e93fb986b792869d
wuqi <1303982968@qq.com>
2018-08-08 15:56:27 +0800

    save

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,29 @@
 <template>
-    <div style="background-color: #EBEBEB;min-height:800px">
-        <div style="width:100%;background-color: #636363; overflow: hidden">
-            <span class="demonstration" style="float:left;padding-top:10px;color:white;margin-left:1%">
-                网站首页
-            </span>
-            <span class="demonstration" style="float:left;padding:5px;color:white;margin-left:2%;width:15%">
-                <el-input
-                        placeholder="请输入"
-                        icon="search"
-                        v-model="searchCriteria"
-                        :on-icon-click="handleIconClick">
+<div style="background-color:#EBEBEB;min-height:800px">
+    <div style="width:100%;background-color:#636363;overflow:hidden">
+        <span class="demonstration" style="float:left;padding-top:10px;color:white;margin-left:1%">
+            网站首页
+        </span>
+        <span class="demonstration" style="float:left;padding:5px;color:white;margin-left:2%;width:15%">
+            <el-input palceholder="请输入" prefix-icon="el-icon-search" v-model="searchCriteria" :on-icon-click="handleIconClick">
                 </el-input>
-            </span>
-            <span class="demonstration" style="float:right;padding-top:10px;margin-right:1%">
-                <el-dropdown trigger="click">
-                  <span class="el-dropdown-link" style="color:white">
+        </span>
+        <span class="demonstration" style="float:right;padding-top:10px;margin-right:1%">
+            <el-dropdown trigger="click">
+                <span class="el-dropdown-link" style="color:white">
                     admin<i class="el-icon-caret-bottom el-icon--right"></i>
-                  </span>
-                  <el-dropdown-menu slot="dropdown">
+                </span>
+                <el-dropdown-menu slot="dropdown">
                     <el-dropdown-item>个人信息</el-dropdown-item>
                     <el-dropdown-item>退出登录</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-            </span>
-        </div>
-
-        <div style="margin-top:5px">
+                </el-dropdown-menu>
+            </el-dropdown>
+        </span>
+    </div>
+    <div style="margin-top:5px">
+        <el-row :gutter="10">
+            <el-col :xs="4" :sm="4"
+      <div style="margin-top:5px">
             <el-row :gutter="10">
                 <el-col :xs="4" :sm="4" :md="4" :lg="4">
                     <div>
